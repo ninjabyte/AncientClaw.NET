e1bf1b2eecb45e1d344e934aa0fe6eb3db09c084
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-09-27 16:23:11 +0200

    Changed folder structure and added example project

---
+++ Claw.NET Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Claw.Imaging;
+using Claw.Imaging.Palettes;
+using Claw.Imaging.Image;
+
+namespace Claw.NET_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Drawing.Image sourceImg = System.Drawing.Image.FromFile("earth.jpg");
+            IImage targetImg = new PaletteImage8((uint)sourceImg.Width, (uint)sourceImg.Height, FullPalette.DefaultColors);
+            ColorConverter.ConvertImage(sourceImg, targetImg);
+            
+            MemoryBitmap targetBmp = targetImg.ToBitmap();
+            targetBmp.Unlock();
+            targetBmp.BitmapImage.Save("out.bmp");
+        }
+    }
+}
